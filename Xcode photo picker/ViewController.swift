219357c0c4589350942f949b7e021e8bde846775
BranfordS813 <sanfordbrandon22@yahoo.com>
2025-10-16 17:19:03 -0400

    Update Main.storyboard and ViewController.swift

--- Xcode photo picker/ViewController.swift
+++ Xcode photo picker/ViewController.swift
@@ -8,10 +8,23 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    // This is the IBOutlet that connects the label to your code
+    @IBOutlet weak var MultiLineLabel1: UILabel!
+    
+    // Lets create a nother IBOutlet that connects a second label to code
+    @IBOutlet weak var MultiLineLabel2: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        // 1. Set the number of lines to 0 (already done in Storyboard, but good to confirm)
+        MultiLineLabel1.numberOfLines = 0
+        
+        // 2. Set the multi-line string using '\n' for line breaks
+        MultiLineLabel2.numberOfLines = 0
+        MultiLineLabel2.text = "App Developed by: \nBrandon Sanford\n\nThis is a test of a multi-line label. Example of third line entry with \\n"
     }
 
 
